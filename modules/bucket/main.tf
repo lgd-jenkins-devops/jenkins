2177d720534f750232ad5e41bf766d454480cf79
Luis <luis@10.10.5.100>
2024-11-07 17:46:47 -0500

    set the bucket module

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "static-site" {
-  name          = "var.name"
-  location      = "var.location"
+  name          = var.name
+  location      = var.location
   force_destroy = true
 
   uniform_bucket_level_access = true
