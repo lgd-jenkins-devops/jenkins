9968219d9d0b4b6e0f046492bd9bf06434aaf326
Luis <luis@10.10.5.100>
2024-11-07 12:30:15 -0500

    set url  map

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -42,24 +42,24 @@ resource "google_compute_url_map" "default" {
 }
 
 # HTTP target proxy
-#resource "google_compute_target_http_proxy" "default" {
-#  name     = "l7-gilb-target-http-proxy"
-#  provider = google
-#  url_map  = google_compute_url_map.default.id
-#}
+resource "google_compute_target_http_proxy" "default" {
+  name     = "l7-gilb-target-http-proxy"
+  provider = google
+  url_map  = google_compute_url_map.default.id
+}
 
 # Reglas de Reenvío (Frontend)
-#resource "google_compute_global_forwarding_rule" "default" {
-#  name       = "http-forwarding-rule"
-#  provider              = google
-#  ip_protocol           = "TCP"
-#  load_balancing_scheme = "EXTERNAL"
-#  port_range = "8080"
-#  target     = google_compute_target_http_proxy.default.self_link
-#  ip_address = google_compute_global_address.default.address
-#}
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "http-forwarding-rule"
+  provider              = google
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range = "8080"
+  target     = google_compute_target_http_proxy.default.self_link
+  ip_address = google_compute_global_address.default.address
+}
 
 # Dirección IP Global
-#resource "google_compute_global_address" "default" {
-#  name = "global-ip"
-#}
+resource "google_compute_global_address" "default" {
+  name = "global-ip"
+}
