c87b4f5f7a14b82679dffa441ea4aad754f487d6
Luis <luis@10.10.5.100>
2024-11-07 14:51:26 -0500

    set https_proxy

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -36,11 +36,16 @@ resource "google_compute_backend_service" "default" {
 }
 
 # Crear el URL Map
-#resource "google_compute_url_map" "default" {
-#  name            = "url-map"
-#  default_service = google_compute_backend_service.default.self_link
-#}
+resource "google_compute_url_map" "default" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.default.self_link
+}
 
+resource "google_compute_target_https_proxy" "default" {
+  name             = "test-proxy"
+  url_map          = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_ssl_certificate.default.id]
+}
 # HTTP target proxy
 #resource "google_compute_target_http_proxy" "default" {
 #  name     = "l7-gilb-target-http-proxy"
