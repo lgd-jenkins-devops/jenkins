ba11f45fbd9019a8d9291b8c8a77271dea66da80
Luis <luis@MacBook.local>
2024-11-11 00:51:44 -0500

    set lb

--- main.tf
+++ main.tf
@@ -49,11 +49,11 @@ module "firewall" {
 }
 
 
-#module "load-balance" {
-#  source = "git@github.com:lgd-jenkins-devops/terraform-modules.git//load_balancer?ref=v1.0.0"
-#  network = module.network.vpc_network_id
-#  jenkins_id = module.vm.vm_id
-#  depends_on = [module.vm]
-#  path_cert = var.ssl.path_cert
-#  path_key = var.ssl.path_key
-#}
+module "load-balance" {
+  source = "git@github.com:lgd-jenkins-devops/terraform-modules.git//load_balancer?ref=v1.0.0"
+  network = module.network.vpc_network_id
+  jenkins_id = module.vm.vm_id
+  depends_on = [module.vm]
+  path_cert = var.ssl.path_cert
+  path_key = var.ssl.path_key
+}
